af22c8ea8876422d936cb7127c7c55e72d0e866f
masiyematome <matomemasiye7@gmail.com>
2021-01-20 18:55:06 +0200

    Changed the nested if-else statements and used a switch case

--- main_app.js
+++ main_app.js
@@ -5,67 +5,76 @@ const carHolders = document.querySelectorAll(".car-holder");
 
 //Event listeners
 
-buttonsContainer.addEventListener("click",showCars);
+buttonsContainer.addEventListener("click", showCars);
 
 //Functions
 
-function showCars(ev){
+function showCars(ev) {
     const clickedButton = ev.target;
 
-    carHolders.forEach(function(holder){
-        if(clickedButton.className == "all-cars-button"){
-            holder.style.display = "flex";
-        }
+    carHolders.forEach(function (holder) {
 
-        else if(clickedButton.className == "sedans-button"){
-            if(holder.classList[1] == "sedan"){
+        switch (clickedButton.className) {
+            
+            case "all-cars-button":
                 holder.style.display = "flex";
-            }
+                break;
 
-            else{
-                holder.style.display = "none";
-            }
-        }
+            case "sedans-button":
+                if (holder.classList[1] == "sedan") {
+                    holder.style.display = "flex";
+                }
 
-        else if(clickedButton.className == "hatchbacks-button"){
-            if(holder.classList[1] == "hatchback"){
-                holder.style.display = "flex";
-            }
+                else {
+                    holder.style.display = "none";
+                }
 
-            else{
-                holder.style.display = "none";
-            }
-        }
+                break;
 
-        else if(clickedButton.className == "coupes-button"){
-            if(holder.classList[1] == "coupe"){
-                holder.style.display = "flex";
-            }
+            case "hatchbacks-button":
+                if (holder.classList[1] == "hatchback") {
+                    holder.style.display = "flex";
+                }
 
-            else{
-                holder.style.display = "none";
-            }
-        }
+                else {
+                    holder.style.display = "none";
+                }
 
-        else if(clickedButton.className == "suvs-button"){
-            if(holder.classList[1] == "suv"){
-                holder.style.display = "flex";
-            }
+                break;
 
-            else{
-                holder.style.display = "none";
-            }
-        }
+            case "coupes-button":
+                if (holder.classList[1] == "coupe") {
+                    holder.style.display = "flex";
+                }
 
-        else if(clickedButton.className == "bakkies-button"){
-            if(holder.classList[1] == "bakkie"){
-                holder.style.display = "flex";
-            }
+                else {
+                    holder.style.display = "none";
+                }
 
-            else{
-                holder.style.display = "none";
-            }
+                break;
+
+            case "suvs-button":
+                if (holder.classList[1] == "suv") {
+                    holder.style.display = "flex";
+                }
+
+                else {
+                    holder.style.display = "none";
+                }
+
+                break;
+
+            case "bakkies-button":
+                if (holder.classList[1] == "bakkie") {
+                    holder.style.display = "flex";
+                }
+
+                else {
+                    holder.style.display = "none";
+                }
+
+                break;
         }
     })
-    
+
 }
