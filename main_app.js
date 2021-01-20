7e19ffc2daf0cf7b49f1e0ee1685498f933b384b
masiyematome <matomemasiye7@gmail.com>
2021-01-20 18:25:05 +0200

    Deleted the code in the function to try a different approach

--- main_app.js
+++ main_app.js
@@ -11,29 +11,7 @@ buttonsContainer.addEventListener("click",showCars);
 
 function showCars(ev){
     const clickedButton = ev.target;
+
     
-    if(clickedButton.className == "all-cars-button"){
-        console.log("All cars will be displayed");
-    }
-
-    else if(clickedButton.className == "hatchbacks-button"){
-        console.log("hatchbacks will be displayed");
-    }
-
-    else if(clickedButton.className == "sedans-button"){
-        console.log("sedans will be displayed");
-    }
-
-    else if(clickedButton.className == "coupes-button"){
-        console.log("coupes will be displayed");
-    }
     
-    else if(clickedButton.className == "suvs-button"){
-        console.log("suvs will be displayed");
-    }
-
-    else if(clickedButton.className == "bakkies-button"){
-        console.log("bakkies will be displayed");
-    }
-
 }
