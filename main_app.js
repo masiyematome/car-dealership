e58b5b8a32582bd0e70e166564c9e37ef1d57670
masiyematome <matomemasiye7@gmail.com>
2021-01-20 18:32:27 +0200

    Added a forEach loop and nested if-else statements inside of it to display the cars

--- main_app.js
+++ main_app.js
@@ -12,6 +12,30 @@ buttonsContainer.addEventListener("click",showCars);
 function showCars(ev){
     const clickedButton = ev.target;
 
-    
+    carHolders.forEach(function(holder){
+        if(clickedButton.className == "all-cars-button"){
+            console.log("I am going to show you all cars");
+        }
+
+        else if(clickedButton.className == "hatchbacks-button"){
+            console.log("I am going to show you all hatchback cars");
+        }
+
+        else if(clickedButton.className == "sedans-button"){
+            console.log("I am going to show you all sedan cars");
+        }
+
+        else if(clickedButton.className == "coupes-button"){
+            console.log("I am going to show you all coupes cars");
+        }
+
+        else if(clickedButton.className == "suvs-button"){
+            console.log("I am going to show you all suvs cars");
+        }
+
+        else if(clickedButton.className == "bakkies-button"){
+            console.log("I am going to show you all bakkies cars");
+        }
+    })
     
 }
